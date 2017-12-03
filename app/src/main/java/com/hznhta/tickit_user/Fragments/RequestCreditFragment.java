24a5309205078fcaa25bd20ca3fcb194c6b9ad62
Hashir Baig <hashirbaig@gmail.com>
2017-12-04 02:31:13 +0500

    UI Fixes & Added loading dialog in RequestCreditFragment

--- app/src/main/java/com/hznhta/tickit_user/Fragments/RequestCreditFragment.java
+++ app/src/main/java/com/hznhta/tickit_user/Fragments/RequestCreditFragment.java
@@ -6,11 +6,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.hznhta.tickit_user.Controllers.RequestCreditController;
 import com.hznhta.tickit_user.Interfaces.OnActionCompletedListener;
@@ -25,6 +27,8 @@ public class RequestCreditFragment extends Fragment {
     @BindView(R.id.id_request_credit_button) Button mRequestCreditButton;
     @BindView(R.id.id_amount) EditText mAmountCredit;
 
+    private AlertDialog mRequestProgressDialog;
+
     public static Fragment newInstance() {
         return new RequestCreditFragment();
     }
@@ -35,18 +39,24 @@ public class RequestCreditFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_request_credit, container, false);
         ButterKnife.bind(this, v);
 
+        mRequestProgressDialog = new AlertDialog.Builder(getActivity())
+                .setTitle("Sending Request...").setView(new ProgressBar(getActivity())).create();
+
         mRequestCreditButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mRequestProgressDialog.show();
                 RequestCreditController.newInstance().requestCredit(Integer.parseInt(mAmountCredit.getText().toString()), new OnActionCompletedListener() {
                     @Override
                     public void onActionSucceed() {
+                        mRequestProgressDialog.dismiss();
                         mAmountCredit.setText("");
                         Snackbar.make(RequestCreditFragment.this.getView(), "Credit Request Sent!", Snackbar.LENGTH_LONG).show();
                     }
 
                     @Override
                     public void onActionFailed(String err) {
+                        mRequestProgressDialog.dismiss();
                         Snackbar.make(RequestCreditFragment.this.getView(), "Unable to send Credit Request!", Snackbar.LENGTH_LONG).show();
                     }
                 });
